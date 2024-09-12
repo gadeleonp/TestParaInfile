4b29a68f262ac136064db29333e4f1bb6331f631
Gustavo de León <gustavo.deleon@gmail.com>
2024-09-12 06:29:22 -0600

    Create LoginModel.cs

---
+++ Model/LoginModel.cs
@@ -0,0 +1,10 @@
+public class LoginModel
+{
+    [Required(ErrorMessage = "El email es requerido.")]
+    [EmailAddress(ErrorMessage = "El email no es válido.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "La contraseña es requerida.")]
+    [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
+    public string Password { get; set; }
+}
