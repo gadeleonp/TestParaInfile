53e64a436ffaaf9f760f72407a321df1a59d29a0
Gustavo de León <gustavo.deleon@gmail.com>
2024-09-12 06:21:55 -0600

    Create User.cs

---
+++ Model/User.cs
@@ -0,0 +1,7 @@
+public class User
+{
+    public string Nombre { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PasswordConfirmation { get; set; }
+}
