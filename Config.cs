bfead51e169c5e23737a185c626e20845c794570
Gustavo de León <gustavo.deleon@gmail.com>
2024-09-12 18:17:47 -0600

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,9 @@
+﻿namespace TestInfileGAdLP
+{
+    public static class Config
+    {
+        public const string ApiBaseUrl = "https://candidates-exam.herokuapp.com/api/v1";
+        public static string Token { get; set; }
+    }
+
+}
---
+++ UsuarioValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TestInfileGAdLP.Models;
+
+namespace TestInfileGAdLP
+{
+    public class UsuarioValidator : AbstractValidator<Usuario>
+    {
+        public UsuarioValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Formato de correo electrónico inválido.");
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8).WithMessage("La contraseña debe tener al menos 8 caracteres.");
+            RuleFor(x => x.Nombre).NotEmpty().WithMessage("El nombre es obligatorio.");
+        }
+    }
+}
